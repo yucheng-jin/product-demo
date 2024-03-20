6b66f28979c9f7b3ddf49a021d145c7d0d6be230
Yucheng Jin <yucheng.jin.sr@gmail.com>
2024-03-20 14:06:11 +0800

    add reflector floor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GroundedSkybox } from 'three/addons/objects/GroundedSkybox';
+// import { GroundedSkybox } from 'three/addons/objects/GroundedSkybox';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader';
+// import { Water } from 'three/addons/objects/Water2';
+import MeshReflectorMaterial from './MeshReflectorMaterial';
 
-let scene, camera, renderer, skybox;
+let scene, camera, renderer, skybox, reflectorFloor;
 
 const materialTable = {
     'Body.001': {
@@ -58,6 +60,9 @@ function init() {
 
 async function setupScene() {
 
+    
+    
+
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
     scene.add(ambientLight);
 
@@ -72,17 +77,20 @@ async function setupScene() {
     const envMap = await hdrLoader.loadAsync('assets/env/blouberg_sunrise_2_1k.hdr');
     envMap.mapping = THREE.EquirectangularReflectionMapping;
 
+    scene.environment = envMap;
+
+    scene.background = new THREE.Color(0x707070);
+    scene.fog = new THREE.Fog(0x707070, 20, 30);
+
     const params = {
         height: 15,
         radius: 100,
         enabled: true,
     };
 
-    skybox = new GroundedSkybox(envMap, params.height, params.radius);
-    skybox.position.y = params.height - 0.01;
-    scene.add(skybox);
-
-    scene.environment = envMap;
+    // skybox = new GroundedSkybox(envMap, params.height, params.radius);
+    // skybox.position.y = params.height - 0.01;
+    // scene.add(skybox);
 
     // load ao textures
 
@@ -91,7 +99,38 @@ async function setupScene() {
     Object.entries(materialTable).forEach(([key, value]) => {
         value.aoMap = textureLoader.load(value.aoMapPath);
         value.aoMap.flipY = false;
-    })
+    });
+
+    // reflecting floor
+
+    reflectorFloor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+    reflectorFloor.material = new MeshReflectorMaterial(renderer, camera, scene, reflectorFloor, {
+        mixStrength: 5,
+        mixBlur: 0,
+        mirror: 0.5,
+    });
+    reflectorFloor.rotation.x = - Math.PI / 2;
+    scene.add(reflectorFloor);
+
+    reflectorFloor.material.color = new THREE.Color(0x707070);
+
+    const floorDiffuseMap = textureLoader.load('assets/textures/FloorsCheckerboard_S_Diffuse.jpg');
+    floorDiffuseMap.wrapS = THREE.RepeatWrapping;
+    floorDiffuseMap.wrapT = THREE.RepeatWrapping;
+    floorDiffuseMap.repeat.set(40, 40);
+    floorDiffuseMap.colorSpace = THREE.SRGBColorSpace;
+    reflectorFloor.material.map = floorDiffuseMap;
+
+    const floorNormalMap = textureLoader.load('assets/textures/FloorsCheckerboard_S_Normal.jpg');
+    floorNormalMap.wrapS = THREE.RepeatWrapping;
+    floorNormalMap.wrapT = THREE.RepeatWrapping;
+    floorNormalMap.repeat.set(40, 40);
+    floorNormalMap.colorSpace = THREE.SRGBColorSpace;
+    reflectorFloor.material.normalMap = floorNormalMap;
+
+    reflectorFloor.material.roughness = 0.5;
+
+    // load car model
 
     new GLTFLoader().load('assets/model/aston_martin.glb', (gltf) => {
         console.log(">>> gltf: ", gltf);
@@ -101,16 +140,15 @@ async function setupScene() {
         gltf.scene.traverse((child) => {
             if (child.isMesh && !child.material.aoMap && !!materialTable[child.material.name]) {
                 child.material.aoMap = materialTable[child.material.name].aoMap;
-                // child.material.specularIntensity = 1.0;
-                // child.material.specularIntensityMap = child.material.specularColorMap;
             }
         })
     })
 
-
 }
 
 function animate() {
+
+    reflectorFloor.material.update();
 
     requestAnimationFrame(animate);
 
@@ -118,11 +156,11 @@ function animate() {
 
 }
 
-(function () {
+(async function () {
 
     init();
 
-    setupScene();
+    await setupScene();
 
     animate();
 
